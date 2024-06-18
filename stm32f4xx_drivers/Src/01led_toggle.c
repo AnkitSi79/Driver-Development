33dd6e877826936bdc26d0c0f166f2650b87b99f
Ankitkumar Singh <ankitkumar.singh@team.satat.tech>
2024-06-18 18:12:31 +0530

    LED toggling with OPENDRAIN Config

--- stm32f4xx_drivers/Src/01led_toggle.c
+++ stm32f4xx_drivers/Src/01led_toggle.c
@@ -20,6 +20,9 @@ int main(void)
 	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
 	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
 	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	/* in open daran config, toggling will not work as thepin will be ground of floting
+	 * to fix the issue GPIO_PinPuPdControl to "GPIO_PIN_PU", if using external pull up then use "GPIO_NO_PUPD"
+	 * if the intensity is low then use extenal pull up with less resitance value */
 	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
 	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
 
